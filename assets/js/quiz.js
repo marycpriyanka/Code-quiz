034028249534fb61aed554d255f5a4bc8d5abb56
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-01-13 12:46:40 -0800

    Add initial html and styling when page loads

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,4 +7,67 @@
  */
 console.log(questions);
 
+let totalQuestions = questions.length;
+let currentQuestionNumber = 0;
+let question = "";
+let choices = [];
+let answer = "";
+
+let timerSection = document.getElementById("time");
+let timer = document.getElementById("timer");
+let containerSection = document.getElementById("container");
+
+document.getElementById("start").addEventListener("click", startQuiz);
+
+function startQuiz()
+{
+    clearContainer();
+    displayQuestion();
+    startTimer();
+}
+
+function clearContainer() {
+    containerSection.innerHTML = "";   
+}
+
+function displayQuestion() {
+    question = questions[currentQuestionNumber].title;
+    choices = questions[currentQuestionNumber].choices;
+
+    let title = document.createElement("h2");
+    title.textContent = question;
+
+    let fieldSet = document.createElement("fieldset");
+
+    let choice;
+    for (let i = 0; i < choices.length; i++)
+    {
+        let option = document.createElement("input");
+        let label = document.createElement("label");
+        let newLine = document.createElement("br");
+
+        choice = choices[i];
+        option.type = "radio";
+        option.id = choice;
+        option.name = "options";
+        option.value = choice;
+        fieldSet.appendChild(option);
+
+        label.textContent = choice;
+        label.for = option.id;
+        fieldSet.appendChild(label);
+
+        fieldSet.appendChild(newLine);
+    }
+
+    containerSection.appendChild(title);
+    containerSection.appendChild(fieldSet);
+}
+
+function startTimer() {
+
+}
+
+
+
 
